15a967d9b1a59538a7ec7872eecad32b67596d71
akljohny <johnyakhil123@gmail.com>
2017-11-09 14:55:53 +0000

    31Task Submitted

---
+++ solution3131.py
@@ -0,0 +1,25 @@
+a = []
+b = []
+x =int(input())
+for i in range(x):
+	a.append(int(input()))
+y = int(input())
+for j in range(y):
+	b.append(int(input())) 
+print(b) 
+
+def ch(varOne):
+	varOne = []
+	#print(varOne)
+ch(a)
+print(a)
+
+
+
+
+#def print_change(varOne):
+ #   varOne = [1,2,3]
+  #  print("VarOne Value in Function",varOne) 
+#varOne = [11,12,13] 
+#print_change(varOne)
+#print("VarOne Value out of Function",varOne)
