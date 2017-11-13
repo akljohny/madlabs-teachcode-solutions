73f6a339784f4c6812c280ed325360b55e424645
akljohny <johnyakhil123@gmail.com>
2017-11-13 12:47:48 +0000

    35Task Submitted

---
+++ solution3535.py
@@ -0,0 +1,12 @@
+def solve():
+	if 6 and 5 != 6:
+		return True
+	else:
+		return False
+if solve() == True:
+
+	add = 5 + 8
+	if add != 11:
+		print("Nearing Completion")
+	else:
+		print("Crack it Fast !")
